f8d5477f73344808d5df3b4b2680b62211b96814
Brian Leip <brianleip@gmail.com>
2020-09-14 18:58:06 -0700

    Completed React workshop 2: task 1

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 
 class CampsiteInfo extends Component {
-  constructor(props) {
-    super(props);
-  }
+
   render() {
-    if (this.props.campsite) {
-      return ( <div className="row"></div> );
-    }
-    else {
-      return ( <div></div> );
-    }
+    if(this.props.campsite) { return <div className="row"></div>; }
+    else { return <div />; }
   }
 }
 
--- 3-React/nucampsite/src/components/DirectoryComponent.js
+++ 3-React/nucampsite/src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Directory extends Component {
   constructor(props) {
@@ -41,16 +42,12 @@ class Directory extends Component {
       );
     });
 
-    return (
+    return (  // this is the main return method from Directory.render()
       <div className="container">
         <div className="row">
           {directory}
         </div>
-        <div className="row">
-          <div className="col-md-5 m1">
-            {this.renderSelectedCampsite(this.state.selectedCampsite)}
-          </div>
-        </div>
+        <CampsiteInfo campsite={this.state.selectedCampsite} />
       </div>
     );
   }
