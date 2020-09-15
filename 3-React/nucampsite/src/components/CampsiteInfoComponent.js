208fc360b47bfd1bc1997b24aec3f5f5e00675cf
Brian Leip <brianleip@gmail.com>
2020-09-14 20:34:36 -0700

    Completed task 3 and done with React workshop week 2

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -3,23 +3,47 @@ import { Card, CardImg, CardText, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
 
-  RenderCampsite(campsite) {
-    return (
-      <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardTitle>{campsite.name}</CardTitle>
-          <CardText>{campsite.description}</CardText>
-        </Card>
-      </div>
-    );
+  renderCampsite(campsite) {
+    if(campsite) {
+      return (
+        <div className="col-md-5 m-1">
+          <Card>
+            <CardImg top src={campsite.image} alt={campsite.name} />
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </Card>
+        </div>
+      );
+    }
+  }
+
+  renderComments(comments) {
+    if(comments) { 
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          { 
+            comments.map(comment => {
+              return (
+                <div>
+                  <p>{comment.text}</p>
+                  <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      ); 
+    }
+    else { return <div />; }
   }
 
   render() {
     if(this.props.campsite) { 
       return (
         <div className="row">
-          {this.RenderCampsite(this.props.campsite)}
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
         </div>
       ); 
     }
