fd223ca821a91f45fa679a77a5b2ab8121bc8a0e
Brian Leip <brianleip@gmail.com>
2020-09-19 10:46:21 -0700

    Array Destructuring on Sat morning workshop

---
+++ 3-React/Exercises/ArrayDestructuring.js
@@ -0,0 +1,45 @@
+// ARRAY DESTRUCTURING
+const myArray = [0, 1, 2, 3];
+console.log(`My array is ${myArray}`);
+
+let someVar = myArray[0];
+console.log(`someVar=${someVar}`);
+
+const [one, two, three, four] = myArray;
+console.log(`one=${one}, two=${two}, three=${three}, four=${four}`);
+
+
+// OBJECT DESTRUCTURING
+const user = {
+  id: 0,
+  name: "Brian",
+  country: "USA",
+  city: "Orange",
+  state: "California"
+};
+// console.log(user);
+
+console.log(`The current user is ${user.name}`);
+
+const {id, name, country, city, state} = user;
+console.log(`id=${id}, name=${name}, city=${city}, state=${state}, country=${country}`);
+
+// GIVE AN OBJECT A DEFAULT VALUE
+const {createdDate = [`09/18/2020`]} = user;
+console.log(user);
+
+// GIVE A PROP AN ALIAS
+const {name: userName} = user;
+console.log(`Your user name is ${userName.toLowerCase()}`);
+
+// GRAB REMAINING UNSPECIFIED VARIABLES
+const user2 = {
+  id2: 0,
+  fName2: "Brian",
+  lName2: "Leip",
+  country2: "USA",
+  city2: "Orange",
+  state2: "California"
+};
+const {fName2, lName2, state2, ...rest} = user2;  // select the variables you need, then use ...someVariableName to grab the remaining unspecified variables
+console.log(rest);
--- 3-React/Exercises/ObjectDestructuring.js
+++ 3-React/Exercises/ObjectDestructuring.js
@@ -4,14 +4,19 @@ const hotel = {
   name: "Hotel Honolulu"
 };
 
-const id = hotel.id;
-const city = hotel.city;
-const name = hotel.name;
+console.log("\n*** EXPLICIT OBJECT VARIABLE EXTRACTION ***")
+const id1 = hotel.id;
+const city1 = hotel.city;
+const name1 = hotel.name;
+console.log(`Hotel ${id1}: ${name1} located in ${city1}`);
+
+console.log("\n*** OBJECT DESTRUCTURING ***")
+const { id2, city2, name2 } = hotel; // object destructing to create new variables (NOTE: does not work. Variable names need to match original variable names)
+console.log(`Hotel ${id2}: ${name2} located in ${city2}`);
+const { id, city, name } = hotel; // object destructing to create new variables
 console.log(`Hotel ${id}: ${name} located in ${city}`);
 
-const { id2, city2, name2 } = hotel; // object destructing to create new variables (EDIT: not working for some reason...)
-console.log(`Hotel ${id2}: ${name2} located in ${city2}`);
-
+console.log("\n*** EXPLICIT FUNCTION PROP EXTRACTION ***")
 function logHotelInfo1(hotel) {
   const id = hotel.id;
   const city = hotel.city;
@@ -20,6 +25,7 @@ function logHotelInfo1(hotel) {
 }
 logHotelInfo1(hotel);
 
+console.log("\n*** OBJECT DESTRUCTURING IN FUNCTION PROPS ***")
 function logHotelInfo2({id, name, city}) {  // object destructuring as function properties
   console.log(`Hotel ${id}: ${name} located in ${city}`);
 }
