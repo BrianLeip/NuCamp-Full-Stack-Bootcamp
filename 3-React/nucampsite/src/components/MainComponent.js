e50025857a6c053192760e7d7112ed41391a0815
Brian Leip <brianleip@gmail.com>
2020-09-20 14:54:26 -0700

    Updated MainComponent to render each CampsiteWithId on the Directory page

--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -31,12 +32,23 @@ class Main extends Component {
                 />
             );
         }
+
+        const CampsiteWithId = ({match}) => {
+            return(
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} // the `+` before `match` is a shortcut to convert string to number
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                />
+            );
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} /> } />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
@@ -48,3 +60,6 @@ class Main extends Component {
 
 export default Main;
 // NOTE: when using Route and need to pass along state data, use render().  Otherwise, use component=
+// NOTE 2: the `:` symbol is very important in `/directory/:campsiteId`.  It tells router that anything after the : is a parameter and
+//         puts the variable inside `campsiteId`. Then the router stores an object called `match` which has params.  The campsiteId
+//         is one of the params of the match object.
