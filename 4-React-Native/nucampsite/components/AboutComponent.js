e712f2f69bbcf4b586e4d6af5c0cd8fece0245fb
Brian Leip <brianleip@gmail.com>
2020-10-24 00:16:36 -0700

    Set up About and Contact components

---
+++ 4-React-Native/nucampsite/components/AboutComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+class About extends Component {
+  render() {
+    return (
+      <ScrollView>
+
+      </ScrollView>
+    );
+  }
+}
+
+export default About;
---
+++ 4-React-Native/nucampsite/components/ContactComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+class Contact extends Component {
+  render() {
+    return (
+      <ScrollView>
+
+      </ScrollView>
+    );
+  }
+}
+
+export default Contact;
