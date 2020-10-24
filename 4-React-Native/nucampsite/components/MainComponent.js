c80c927a146bbd122c18f4cf2e9744f734931b9d
Brian Leip <brianleip@gmail.com>
2020-10-24 03:18:41 -0700

    Completed ex: Navigation Icons and Custom Slide Drawer

--- 4-React-Native/nucampsite/components/MainComponent.js
+++ 4-React-Native/nucampsite/components/MainComponent.js
@@ -4,11 +4,12 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
-import { View, Platform, StyleSheet } from 'react-native';
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+import SafeAreaView from 'react-native-safe-area-view';
 
 const ContactNavigator = createStackNavigator(
   {
@@ -92,6 +93,28 @@ const HomeNavigator = createStackNavigator(
   }
 );
 
+const customDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{top: 'always', horizontal: 'never'}}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{flex: 1}}>
+          <Image
+            source={require('./images/logo.png')}
+            style={styles.drawerImage}
+          />
+        </View>
+        <View style={{flex: 2}}>
+          <Text style={styles.drawerHeaderText}>NuCamp</Text>
+        </View>
+      </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
+
 const MainNavigator = createDrawerNavigator(
   {
     Home: { 
@@ -150,7 +173,8 @@ const MainNavigator = createDrawerNavigator(
     }
   },
   {
-    drawerBackgroundColor: '#CEC8FF'
+    drawerBackgroundColor: '#CEC8FF',
+    contentComponent: customDrawerContentComponent
   }
 );
 
@@ -171,6 +195,26 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  drawerHeader: {
+    backgroundColor: '#5637DD',
+    height: 140,
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  drawerHeaderText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },  
+  drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60
+  },
   stackIcon: {
     marginLeft: 10,
     color: '#fff',
