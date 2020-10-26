249e0c560ed03ff174b2d5e405b51ac2868dd6f9
Brian Leip <brianleip@gmail.com>
2020-10-26 02:01:31 -0700

    Finished connecting the Loading Activity indicator to Home, Directory, About

--- 4-React-Native/nucampsite/components/AboutComponent.js
+++ 4-React-Native/nucampsite/components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
