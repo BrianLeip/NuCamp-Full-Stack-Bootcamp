6ace2d1ebd710923399983f4d917307f27039491
Brian Leip <brianleip@gmail.com>
2020-11-20 21:56:50 -0800

    Completed ex: Express router: moved routing to campsiteRouter.js

---
+++ 5-NodeJS-Express-MongoDB/node-express/routes/campsiteRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const campsiteRouter = express.Router();
+
+campsiteRouter.route('/')           // chaining all verbs to this router below
+.all((req, res, next) => {          // .all is catch all for all HTTP verbs
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+})
+.get((req, res) => {
+  res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+  res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+  res.end('Deleting all campsites');
+});
+
+module.exports = campsiteRouter;
