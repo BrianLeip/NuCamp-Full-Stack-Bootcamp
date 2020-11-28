fd5b28d466bf2bc9fba14ef8fc705c220596dcca
Brian Leip <brianleip@gmail.com>
2020-11-28 10:28:09 -0800

    Updated app.js to use mongoose and connect to the database

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -3,12 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionsRouter = require('./routes/promotionsRouter');
 const partnersRouter = require('./routes/partnersRouter');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+connect.then(() => {
+  console.log('Connected correctly to server'),
+  err => console.log(err)
+});
 
 var app = express();
 
