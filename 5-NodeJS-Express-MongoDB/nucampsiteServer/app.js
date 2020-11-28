c4e96d51dc15e89d7edd976e1721ca46586ec77d
Brian Leip <brianleip@gmail.com>
2020-11-27 21:04:02 -0800

    fixed plural 's' on routers

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -7,8 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
-const promotionRouter = require('./routes/promotionRouter');
-const partnerRouter = require('./routes/partnerRouter');
+const promotionsRouter = require('./routes/promotionsRouter');
+const partnersRouter = require('./routes/partnersRouter');
 
 var app = express();
 
@@ -25,8 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+app.use('/promotions', promotionsRouter);
+app.use('/partners', partnersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
