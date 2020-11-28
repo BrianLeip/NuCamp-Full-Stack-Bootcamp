35905aa32f68325f291926b89a1d6ec7204fd609
Brian Leip <brianleip@gmail.com>
2020-11-28 02:09:24 -0800

    updated operations.js to use promises instead of nested callbacks

--- 5-NodeJS-Express-MongoDB/node-mongo/operations.js
+++ 5-NodeJS-Express-MongoDB/node-mongo/operations.js
@@ -1,34 +1,21 @@
 const assert = require('assert').strict;
 
-exports.insertDocument = (db, document, collection, callback) => {
-  const coll = db.collection(collection); // gets the collection based on the string passed in to 'collection'
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result); // passes the result on to the callback function that was included in the parameters
-  });
+exports.insertDocument = (db, document, collection) => {
+  const coll = db.collection(collection);
+  return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
-  const coll = db.collection(collection); 
-  coll.find().toArray((err, docs) => {  // find all collections and convert to an array
-    assert.strictEqual(err, null);
-    callback(docs);
-  });  
+exports.findDocuments = (db, collection) => {
+  const coll = db.collection(collection);
+  return coll.find({}).toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
-  const coll = db.collection(collection); 
-  coll.deleteOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+exports.removeDocument = (db, document, collection) => {
+  const coll = db.collection(collection);
+  return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
-  // console.log("Updating document: ", document);
-  const coll = db.collection(collection); 
-  coll.updateOne(document, { $set: update }, null, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+exports.updateDocument = (db, document, update, collection) => {
+  const coll = db.collection(collection);
+  return coll.updateOne(document, { $set: update }, null);
 };
