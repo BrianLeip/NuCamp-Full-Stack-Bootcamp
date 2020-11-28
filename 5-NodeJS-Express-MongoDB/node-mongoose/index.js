affeaa9e311fc5f85ff36edd46b71a308074a454
Brian Leip <brianleip@gmail.com>
2020-11-28 09:59:12 -0800

    Completed ex: mongoose ODM part 2

--- 5-NodeJS-Express-MongoDB/node-mongoose/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -10,14 +10,24 @@ const connect = mongoose.connect(url, {
 
 connect.then(() => {
   console.log('Connected successfully to the server');
-  const newCampsite = new Campsite({
-    name: 'React Lake Campground',
-    description: 'test'             // note - if this description was excluded, mongoose would throw an error since it needs to match the Schema
-  });
+  // const newCampsite = new Campsite({
+  //   name: 'React Lake Campground',
+  //   description: 'test'             // note - if this description was excluded, mongoose would throw an error since it needs to match the Schema
+  // });
+  // newCampsite.save()
+  // .then(campsite => {
+  //   console.log("Saving campsite: " + campsite);
+  //   return Campsite.find();
+  // })
 
-  newCampsite.save()
+  console.log("Creating new campsite 'React Lake Campground'...");
+  Campsite.create({
+    name: 'React Lake Campground',
+    description: 'test'
+  })
   .then(campsite => {
-    console.log("Saving campsite: " + campsite);
+    console.log("Finding campsite: ");
+    console.log(campsite);
     return Campsite.find();
   })
   .then(campsites => {
